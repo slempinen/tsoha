a32f6b414197b82231ef1bef0a07d5c2d6cc1eb8
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-02-20 16:37:11 +0200

    Add forums to frontend

--- flaskr/forum.py
+++ flaskr/forum.py
@@ -4,9 +4,26 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
+from flaskr.db import db
 
 bp = Blueprint('forum', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('forum/index.html')
+     getForums = 'SELECT * FROM forum'
+     forums = db.session.execute(getForums).fetchall()
+     return render_template('forum/index.html', forums=forums)
+
+@bp.route('/forum/<int:forum_id>')
+def forum(forum_id):
+     getForum = 'SELECT * FROM forum WHERE id = (:forum_id)'
+     forum = db.session.execute(getForum, { "forum_id": forum_id }).fetchone()
+
+     getTopics = 'SELECT * FROM topic WHERE forum_id = (:forum_id)'
+     topics = db.session.execute(getTopics, { "forum_id": forum_id }).fetchall()
+
+     return render_template('forum/forum.html', title=forum.name, topics=topics)
+
+@bp.route('/forum/<int:forum_id>/topic/<int:topic_id>')
+def topic(forum_id):
+     pass
