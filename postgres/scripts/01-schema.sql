4364ef1017e3b73bf055499891c9448a3ef909ec
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-03-06 15:23:48 +0200

    Fix foreign key data type

--- postgres/scripts/01-schema.sql
+++ postgres/scripts/01-schema.sql
@@ -3,34 +3,32 @@ CREATE TABLE account
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE  NOT NULL,
     password VARCHAR NOT NULL,
-    email    VARCHAR(50) UNIQUE NOT NULL
+    email    VARCHAR(50) UNIQUE NOT NULL,
+    is_admin BOOLEAN
 );
 
 CREATE TABLE forum
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL,
-    description TEXT
+    description TEXT,
+    creator_account INTEGER REFERENCES account(id),
+    private BOOLEAN NOT NULL DEFAULT false,
+    password VARCHAR
 );
 
-CREATE TABLE private_forum
-(
-    id SERIAL PRIMARY KEY,
-    forum_id SERIAL REFERENCES forum(id) NOT NULL,
-    password VARCHAR(50) NOT NULL
-);
 
 CREATE TABLE private_forum_account
 (
-    account_id SERIAL REFERENCES account(id) NOT NULL,
-    forum_id SERIAL REFERENCES private_forum(id) NOT NULL
+    account_id INTEGER REFERENCES account(id) NOT NULL,
+    forum_id INTEGER REFERENCES forum(id) NOT NULL
 );
 
 CREATE TABLE topic
 (
     id SERIAL PRIMARY KEY,
-    forum_id SERIAL REFERENCES forum(id) NOT NULL,
-    account_id SERIAL REFERENCES account(id) NOT NULL,
+    forum_id INTEGER REFERENCES forum(id) NOT NULL,
+    account_id INTEGER REFERENCES account(id) NOT NULL,
     title VARCHAR(30),
     body TEXT NOT NULL
 );
@@ -38,8 +36,8 @@ CREATE TABLE topic
 CREATE TABLE comment
 (
     id SERIAL PRIMARY KEY,
-    account_id SERIAL REFERENCES account(id) NOT NULL,
-    topic_id SERIAL REFERENCES topic(id) NOT NULL,
+    account_id INTEGER REFERENCES account(id) NOT NULL,
+    topic_id INTEGER REFERENCES topic(id) NOT NULL,
     body TEXT
 );
 
