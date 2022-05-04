2bca31a35e4d2b14b670c902c4049e914e365c56
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-05-04 20:25:59 +0300

    Remove pg admin from docker-compose, change pg container exposed port

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
                 SECRET_KEY='dev',
-                SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:password123@localhost:4321/postgres',
+                SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:password123@localhost:5432/postgres',
                 SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
