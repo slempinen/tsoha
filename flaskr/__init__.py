3d62775779b959c78e1e195404ecf7632a5a83eb
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-05-07 20:16:57 +0300

    Configure app for production

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,17 +4,26 @@ from .db import db
 from . import auth
 from . import search
 from .forum import forum_blueprint
+from dotenv import load_dotenv
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-                SECRET_KEY='dev',
-                SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:password123@localhost:5432/postgres',
-                SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )
 
-    app.config.from_pyfile('config.py', silent=True)
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    if FLASK_ENV == 'development':
+        app.config.from_mapping(
+          SECRET_KEY='dev',
+          SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:password123@localhost:5432/postgres',
+          SQLALCHEMY_TRACK_MODIFICATIONS=False
+        )
+    else:
+        app.config.from_mapping(
+          SECRET_KEY=os.environ.get('SECRET_KEY'),
+          SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+          SQLALCHEMY_TRACK_MODIFICATIONS=False
+        )
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
