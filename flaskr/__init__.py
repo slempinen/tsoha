86b0a418fe211e33f30feaac668266ed83c5e367
Santtu Lempinen <santtu.lempinen@nokia.com>
2022-05-07 20:54:11 +0300

    Fix db uri for heroku

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -18,9 +18,12 @@ def create_app(test_config=None):
           SQLALCHEMY_TRACK_MODIFICATIONS=False
         )
     else:
+        DATABASE_URL = os.getenv("DATABASE_URL")  # or other relevant config var
+        if DATABASE_URL.startswith("postgres://"):
+            DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
         app.config.from_mapping(
           SECRET_KEY=os.environ.get('SECRET_KEY'),
-          SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL'),
+          SQLALCHEMY_DATABASE_URI=DATABASE_URL,
           SQLALCHEMY_TRACK_MODIFICATIONS=False
         )
 
